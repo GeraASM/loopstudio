a42a670234e47f47ae6daa101116b15926c59c50
GeraASM <gera2002h@gmail.com>
2025-06-17 16:34:32 -0600

    project viewport finish

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const bg = document.getElementById('background');
 const interactiveImg = document.querySelector('.interactive__img');
+const creationsImg = document.querySelectorAll('.creations__img');
 
 function newBackground() {
 
@@ -9,11 +10,17 @@ function newBackground() {
 
     if (window.innerWidth >= 1024) {
         bg.style.backgroundImage = "url('./images/desktop/image-hero.jpg')";
+        creationsImg.forEach((creationImg, index) => {
+            creationImg.src = creationsDesktop[index];
+        })
+
 
     } else if (window.innerWidth < 768) {
         bg.style.backgroundImage = "url('./images/mobile/image-hero.jpg')";
         interactiveImg.src = './images/mobile/image-interactive.jpg';
-
+        creationsImg.forEach((creationImg, index) => {
+            creationImg.src = creationsMobile[index];
+        })
     }
 }
 
@@ -21,3 +28,26 @@ function newBackground() {
 
 window.addEventListener('DOMContentLoaded', newBackground);
 window.addEventListener('resize', newBackground);
+
+const creationsDesktop = {
+    0: './images/desktop/image-deep-earth.jpg',
+    1: './images/desktop/image-night-arcade.jpg',
+    2: './images/desktop/image-soccer-team.jpg',
+    3: './images/desktop/image-grid.jpg',
+    4: './images/desktop/image-from-above.jpg',
+    5: './images/desktop/image-pocket-borealis.jpg',
+    6: './images/desktop/image-curiosity.jpg',
+    7: './images/desktop/image-fisheye.jpg'
+}
+
+const creationsMobile = {
+    0: './images/mobile/image-deep-earth.jpg',
+    1: './images/mobile/image-night-arcade.jpg',
+    2: './images/mobile/image-soccer-team.jpg',
+    3: './images/mobile/image-grid.jpg',
+    4: './images/mobile/image-from-above.jpg',
+    5: './images/mobile/image-pocket-borealis.jpg',
+    6: './images/mobile/image-curiosity.jpg',
+    7: './images/mobile/image-fisheye.jpg'
+}
+
